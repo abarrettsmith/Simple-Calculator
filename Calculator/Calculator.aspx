5e3aadf54e634b91e6c6737e6ceda4ae79b81c47
Adam Smith <adamsmith601@hotmail.com>
2016-05-30 18:44:08 -0400

    Simple Calculator
    Can only calculate two values at a time

---
+++ Calculator/Calculator.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="Calculator.Calculator"  %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <table>
+                <tr>
+                    <td>
+                        <asp:TextBox ID="InputField" runat="server" Text="" Enabled="false"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="BTNSeven" runat="server" Width="30px" Text="7" CommandArgument="7" OnClick="BTN_Add_Value_Click" />
+                        <asp:Button ID="BTNEight" runat="server" Width="30px" Text="8" CommandArgument="8" OnClick="BTN_Add_Value_Click" />
+                        <asp:Button ID="BTNNine" runat="server" Width="30px" Text="9" CommandArgument="9" OnClick="BTN_Add_Value_Click" />
+                        <asp:Button ID="BTNPlus" runat="server" Width="30px" Text="+" CommandArgument="+" OnClick="BTN_Operator_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="BTNFour" runat="server" Width="30px" Text="4" CommandArgument="4" OnClick="BTN_Add_Value_Click" />
+                        <asp:Button ID="BTNFive" runat="server" Width="30px" Text="5" CommandArgument="5" OnClick="BTN_Add_Value_Click" />
+                        <asp:Button ID="BTNSix" runat="server" Width="30px" Text="6" CommandArgument="6" OnClick="BTN_Add_Value_Click" />
+                        <asp:Button ID="BTNMinus" runat="server" Width="30px" Text="-" CommandArgument="-" OnClick="BTN_Operator_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="BTNOne" runat="server" Width="30px" Text="1" CommandArgument="1" OnClick="BTN_Add_Value_Click" />
+                        <asp:Button ID="BTNTwo" runat="server" Width="30px" Text="2" CommandArgument="2" OnClick="BTN_Add_Value_Click" />
+                        <asp:Button ID="BTNThree" runat="server" Width="30px" Text="3" CommandArgument="3" OnClick="BTN_Add_Value_Click" />
+                        <asp:Button ID="BTNMultiply" runat="server" Width="30px" Text="x" CommandArgument="x" OnClick="BTN_Operator_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="BTNZero" runat="server" Width="30px" Text="0" CommandArgument="0" OnClick="BTN_Add_Value_Click" />
+                        <asp:Button ID="BTNClear" runat="server" Width="30px" Text="C" OnClick="BTN_Clear_Click" />
+                        <asp:Button ID="BTNEquals" runat="server" Width="30px" Text="=" OnClick="BTN_Equals_Click" />
+                        <asp:Button ID="BTNDivide" runat="server" Width="30px" Text="/" CommandArgument="/" OnClick="BTN_Operator_Click" />
+                    </td>
+                </tr>
+            </table>
+
+            <asp:Label ID="ExceptionHandler" runat="server" Text=""></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Calculator/Calculator.aspx.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        private string number;
+        private string currentOperator;
+        private bool lastClickOperator = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // make the buttons colorful
+            BTNZero.BackColor = System.Drawing.Color.LightGray;
+            BTNOne.BackColor = System.Drawing.Color.LightGray;
+            BTNTwo.BackColor = System.Drawing.Color.LightGray;
+            BTNThree.BackColor = System.Drawing.Color.LightGray;
+            BTNFour.BackColor = System.Drawing.Color.LightGray;
+            BTNFive.BackColor = System.Drawing.Color.LightGray;
+            BTNSix.BackColor = System.Drawing.Color.LightGray;
+            BTNSeven.BackColor = System.Drawing.Color.LightGray;
+            BTNEight.BackColor = System.Drawing.Color.LightGray;
+            BTNNine.BackColor = System.Drawing.Color.LightGray;
+            BTNPlus.BackColor = System.Drawing.Color.DarkGray;
+            BTNMinus.BackColor = System.Drawing.Color.DarkGray;
+            BTNMultiply.BackColor = System.Drawing.Color.DarkGray;
+            BTNDivide.BackColor = System.Drawing.Color.DarkGray;
+            BTNEquals.BackColor = System.Drawing.Color.DarkGray;
+            BTNClear.BackColor = System.Drawing.Color.DarkOrange;
+            
+            // if this is a postback request reset the local variables to their ViewState value
+            if (this.IsPostBack)
+            {
+                number = (string)ViewState["number"];
+                currentOperator = (string)ViewState["currentOperator"];
+                lastClickOperator = (bool)ViewState["lastClickOperator"];
+            }
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            // on page render assign the local variables to a ViewState
+            ViewState["number"] = number;
+            ViewState["currentOperator"] = currentOperator;
+            ViewState["lastClickOperator"] = lastClickOperator;
+        }
+
+        protected void BTN_Add_Value_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // add button value to textfield
+                Button btn = (Button)sender;
+                InputField.Text += btn.CommandArgument;
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.BackColor = System.Drawing.Color.Red;
+                ExceptionHandler.Text = "An error occured on the 'BTN_Add_Value' event. Exception Message: " + ex.Message;
+            }
+        }
+
+        protected void BTN_Operator_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // add the operator to the textfield. if one has already been clicked replace the old operator with the new one
+                Button btn = (Button)sender;
+                InputField.Enabled = true;
+                if (!lastClickOperator)
+                {
+                    currentOperator = btn.CommandArgument;
+                    number = InputField.Text;
+                    InputField.Text = number.ToString() + " " + btn.CommandArgument + " ";
+                }
+                else
+                {
+                    InputField.Text = InputField.Text.Replace(currentOperator, btn.CommandArgument);
+                }
+                currentOperator = btn.CommandArgument;
+                InputField.Enabled = false;
+                lastClickOperator = true;
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.BackColor = System.Drawing.Color.Red;
+                ExceptionHandler.Text = "An error occured on the 'BTN_Operator' event. Exception Message: " + ex.Message;
+            }
+        }
+
+        protected void BTN_Equals_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // if an operator has been clicked run calculation
+                InputField.Enabled = true;
+                if (lastClickOperator)
+                {
+                    // assign our numbers to two seperate variables
+                    int secondNumberLastIndex = InputField.Text.LastIndexOf(" ") + 1;
+                    string secondNumberString = InputField.Text.Substring(secondNumberLastIndex, InputField.Text.Length - secondNumberLastIndex);
+                    int FirstNumber = Convert.ToInt32(number);
+                    int SecondNumber = Convert.ToInt32(secondNumberString);
+
+                    int total = 0;
+
+                    // find which operator has been selected and calculate accordingly
+                    if (currentOperator == "+")
+                    {
+                        total = FirstNumber + SecondNumber;
+                    }
+                    else if (currentOperator == "-")
+                    {
+                        total = FirstNumber - SecondNumber;
+                    }
+                    else if (currentOperator == "x")
+                    {
+                        total = FirstNumber * SecondNumber;
+                    }
+                    else if (currentOperator == "/")
+                    {
+                        if (FirstNumber == 0 || SecondNumber == 0)
+                        {
+                            total = 0;
+                        }
+                        else
+                        {
+                            total = FirstNumber / SecondNumber;
+                        }
+                    }
+
+                    InputField.Text = total.ToString();
+                }
+                InputField.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.BackColor = System.Drawing.Color.Red;
+                ExceptionHandler.Text = "An error occured on the 'BTN_Equals' event. Exception Message: " + ex.Message;
+            }
+        }
+
+        protected void BTN_Clear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // reset the calculator
+                InputField.Text = "";
+                number = null;
+                currentOperator = null;
+                lastClickOperator = false;
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.BackColor = System.Drawing.Color.Red;
+                ExceptionHandler.Text = "An error occured on the 'BTN_Clear' event. Exception Message: " + ex.Message;
+            }
+        }
+    }
+}
